d25e5a4519bd4c1ff65d3b5f28274a819d708c2d
tonythomasm6 <68453981+tonythomasm6@users.noreply.github.com>
2020-09-04 10:52:00 +1000

    Week5-Webservice Single project commit

--- W5_WebserviceComplete/src/com/client/WebServiceClient.java
+++ W5_WebserviceComplete/src/com/client/WebServiceClient.java
@@ -12,7 +12,7 @@ public class WebServiceClient {
 	
 	private WebTarget webTarget;
     private Client client;
-    private static final String BASE_URI = "http://0.0.0.0:9080/W5_WebserviceAdd/webresources";
+    private static final String BASE_URI = "http://0.0.0.0:9080/W5_WebserviceComplete/webresources";
     
 	public WebServiceClient() {
 		client = javax.ws.rs.client.ClientBuilder.newClient();
